50a4f73caedd7e3e715feb6721f9f1e14cc43e61
Linh Hoang <linhoang.ce@gmail.com>
2026-02-01 21:17:17 -0800

    vercel fix

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const path = require("path");
 const ServerTime = require("./myModules/utils");
 const userMessage = require("./lang/en/en");
 
-http.createServer((req, res) => {
+// 1. Define the handler function
+const serverHandler = (req, res) => {
 	const baseUrl = `https://${req.headers.host}`;
 	const myUrl = new URL(req.url, baseUrl);
 	const pathname = myUrl.pathname;
@@ -13,15 +14,14 @@ http.createServer((req, res) => {
 	// Normalize path to handle leading/trailing slashes
 	const cleanPath = pathname.replace(/\/$/, "");
 
+	// GET DATE
 	if (cleanPath.endsWith("/getDate")) {
-		const name = myUrl.searchParams.get("name");
+		const name = myUrl.searchParams.get("name") || "Guest";
 		const datetime = new ServerTime();
 		let template = userMessage.template;
 		const finalMessage = template.replace("%1", name).replace("%2", datetime.getCurrentTime());
 
-		res.writeHead(200, {
-			"Content-Type": "text/html",
-		});
+		res.writeHead(200, { "Content-Type": "text/html" });
 		res.write(`<div style='color:blue'>${finalMessage}</div>`);
 		return res.end();
 	}
@@ -29,18 +29,15 @@ http.createServer((req, res) => {
 	// WRITE FILE
 	else if (cleanPath.endsWith("/writeFile")) {
 		const text = myUrl.searchParams.get("text") + "\n";
-		const filePath = path.join(__dirname, "file.txt");
+		// Using /tmp/ is the ONLY way to (temporarily) write files on Vercel
+		const filePath = path.join("/tmp", "file.txt");
 
 		fs.appendFile(filePath, text, (error) => {
 			if (error) {
-				res.writeHead(404, {
-					"Content-Type": "text/html",
-				});
+				res.writeHead(500, { "Content-Type": "text/html" });
 				return res.end("Error writing to file");
 			}
-			res.writeHead(200, {
-				"Content-Type": "text/html",
-			});
+			res.writeHead(200, { "Content-Type": "text/html" });
 			return res.end(`Successfully appended: ${text}`);
 		});
 	}
@@ -48,18 +45,15 @@ http.createServer((req, res) => {
 	// READ FILE
 	else if (cleanPath.includes("/readFile/")) {
 		const filename = pathname.split("/").pop();
-		const filePath = path.join(__dirname, filename);
+		// Remember to read from the same place you wrote to!
+		const filePath = path.join("/tmp", filename);
 
 		fs.readFile(filePath, "utf8", (err, data) => {
 			if (err) {
-				res.writeHead(404, {
-					"Content-Type": "text/html",
-				});
+				res.writeHead(404, { "Content-Type": "text/html" });
 				return res.end(filename + " 404 Not Found!");
 			}
-			res.writeHead(200, {
-				"Content-Type": "text/html",
-			});
+			res.writeHead(200, { "Content-Type": "text/html" });
 			res.write(`<pre>${data}</pre>`);
 			return res.end();
 		});
@@ -67,4 +61,13 @@ http.createServer((req, res) => {
 		res.writeHead(404);
 		res.end("Page not found!");
 	}
-});
+};
+
+// 2. Export for Vercel
+module.exports = serverHandler;
+
+// 3. Local testing logic (Fixes the 'app' error)
+if (process.env.NODE_ENV !== "production") {
+	const server = http.createServer(serverHandler);
+	server.listen(8888, () => console.log("Running locally on 8888"));
+}
